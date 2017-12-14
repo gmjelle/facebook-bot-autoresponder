5e168d5761a23c5ce552f96b749aa8e0b19e48af
Jelle <jelle@Jelles-MacBook-Pro.local>
2017-12-14 15:59:42 +0100

    first commit 🍾🍾🍾

---
+++ autoresponder.py
@@ -0,0 +1,162 @@
+# Need more info? Check out the blogpost:
+# how-to-make-a-bot-that-automatically-replies-to-comments-on-facebook-post
+
+"""
+NEED MORE INFO? CHECK OUT THE BLOGPOST
+https://thegrowthrevolution.com/how-to-make-a-bot-that-automatically-replies-to-comments-on-facebook-post
+"""
+
+import sqlite3
+from time import sleep
+import facebook
+from PIL import Image
+from io import BytesIO
+
+
+APP_ID = 'TO FILL IN'
+APP_SECRET = 'TO FILL IN'
+
+PAGE_ID = 'TO FILL IN'
+POST_ID_TO_MONITOR = 'TO FILL IN'
+LONG_LIVED_ACCESS_TOKEN = 'TO FILL IN'
+
+COMBINED_POST_ID_TO_MONITOR = '%s_%s' % (PAGE_ID, POST_ID_TO_MONITOR)
+
+
+def make_new_profile_pic(img):
+    im = Image.open(BytesIO(img))
+    im = im.resize((480, 480))
+
+    # background version
+    # background = Image.open("./fcdk_overlay.png")
+    #
+    # background.paste(im, (100, 100))
+
+    # background.show()
+    # bytes_array = BytesIO()
+    # background.save(bytes_array, format='PNG')
+    # bytes_array = bytes_array.getvalue()
+    # return bytes_array
+
+    # foreground version
+    foreground = Image.open("./tgr_overlay.png")
+    foreground = foreground.resize((250, 250))
+    im = im.resize((250, 250))
+
+    im.paste(foreground, (0, 0), foreground)
+
+    # im.show()
+    bytes_array = BytesIO()
+    im.save(bytes_array, format='PNG')
+    bytes_array = bytes_array.getvalue()
+    return bytes_array
+
+
+def comment_on_comment(graph, comment):
+    comment_id = comment['id']
+    comment_from_name = comment['from']['name']
+    comment_from_id = comment['from']['id']
+    comment_message = comment['message']
+    profile = None
+
+    print("Let's comment!")
+    # like the comment
+    graph.put_like(object_id=comment_id)
+
+    # profile info:
+    photo = graph.get_connections(id=comment_from_id, connection_name='picture', height=480, width=480)
+    # try to get first name, if it's a page there is not first_name
+    try:
+        profile = graph.get_object(comment_from_id, fields='first_name,last_name')
+    except:
+        pass
+
+    # let's create our photo that we want to post
+    photo_to_post = make_new_profile_pic(photo['data'])
+
+    # first we upload it as an unpublished photo that doesn't appear as a story (in the newsfeed)
+    posted_photo = graph.put_photo(
+        image=photo_to_post,
+        album_path='404896546596495/photos',
+        no_story=True,
+        published=False
+    )
+
+    # if it's a person that commented, we can use the first name
+    if profile:
+        graph.put_object(parent_object=comment_id, connection_name='comments',
+                         message='Bozour %s. Hier is uwe foto, mijn gedacht!' % (profile['first_name']),
+                         attachment_id=posted_photo['id']
+                         )
+    else:
+        graph.put_object(parent_object=comment_id, connection_name='comments',
+                         message='Hola copain. Hier is uwe foto, mijn gedacht!',
+                         attachment_id=posted_photo['id']
+                         )
+
+    print('Edited and posted the photo of: %s' % comment_from_name)
+
+
+def monitor_fb_comments():
+    # create graph
+    graph = facebook.GraphAPI(LONG_LIVED_ACCESS_TOKEN)
+    # that infinite loop tho
+    while True:
+        print('I spy with my little eye...🕵️ ')
+        sleep(5)
+
+        # get the comments
+        comments = graph.get_connections(COMBINED_POST_ID_TO_MONITOR,
+                                         'comments',
+                                         order='chronological')
+
+        for comment in comments['data']:
+
+            # if we can't find it in our comments database, it means
+            # we haven't commented on it yet
+            if not Posts().get(comment['id']):
+                comment_on_comment(graph, comment)
+
+                # add it to the database, so we don't comment on it again
+                Posts().add(comment['id'])
+
+        # while there is a paging key in the comments, let's loop them and do exactly the same
+        # if you have a better way to do this, PRs are welcome :)
+        while 'paging' in comments:
+            comments = graph.get_connections(COMBINED_POST_ID_TO_MONITOR,
+                                             'comments',
+                                             after=comments['paging']['cursors']['after'],
+                                             order='chronological')
+
+            for comment in comments['data']:
+
+                if not Posts().get(comment['id']):
+                    comment_on_comment(graph, comment)
+                    Posts().add(comment['id'])
+
+
+# Mary had a little class
+class Posts:
+    def __init__(self):
+        self.connection = sqlite3.connect('comments.sqlite3')
+        self.cursor = self.connection.cursor()
+
+    def get(self, id):
+        self.cursor.execute("SELECT * FROM comments where id='%s'" % id)
+
+        row = self.cursor.fetchone()
+
+        return row
+
+    def add(self, id):
+        try:
+            self.cursor.execute("INSERT INTO comments VALUES('%s')" % id)
+            lid = self.cursor.lastrowid
+            self.connection.commit()
+            return lid
+        except sqlite3.IntegrityError:
+            return False
+
+# started at the bottom, etc
+if __name__ == '__main__':
+    monitor_fb_comments()
---
+++ readme.md
@@ -0,0 +1,35 @@
+
+<h1>⚡ AUTORESPONDER FOR FACEBOOK</h1>
+
+<img src="https://thumbs.gfycat.com/ConcreteDopeyCoati-size_restricted.gif"  />
+Created by Jelle VDV
+
+
+----------
+
+
+Repo for all the code talked about in this blogpost: (todo).
+
+This code monitors all the comments of a Facebook post, and then comments on the comments with a custom photo.
+In this code there is an example on how you can make it respond with an image that includes the orginal commenter's profile picture.
+
+This code is just a demo :) Especially with HTTP requests there should be some error handling. But that shouldn't be too hard to implement!
+## Requirements
+
+- Python 3 and all the requirements in the requirements.txt file
+- A (published) Facebook page
+- A Facebook app
+- An access token (In the blogpost you'll learn how to get a long-lived access token)
+
+## Accompanying blogpost
+
+Need more info? I created a whole blogpost to explain the code and how you can change it to suit your needs.
+
+[Link to the blogpost](todo)
+
+## License
+This code is released under MIT license
+
+## Thanks
+Special thanks to Inti De Ceukelaire, who was the first one to write (I think) to write such a tool. 
+You can find more info about how he implemented it [here (in Dutch)](https://medium.com/cookiesclub/de-robotrevolte-studio-brussel-nam-de-eerste-facebookbot-in-dienst-81de19d6ed6d).
